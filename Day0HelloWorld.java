9f1a6adc41aaf501be2314d589a656b220f89b36
Shubham Garg <shubham27dec@gmail.com>
2022-10-09 00:51:11 +0530

    Update Day0HelloWorld.java

--- Day0HelloWorld.java
+++ Day0HelloWorld.java
@@ -5,7 +5,7 @@ public class Day0HelloWorld {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String inputString = sc.nextLine();
-        System.out.println("Hello, World.");
+        System.out.println("Hello, World!");
         System.out.println(inputString);
     }
 }
